c33e02dc78183106421d7c5f8067abc08795a771
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-09-21 18:19:56 -0500

    added tech stack and description into project cards, changed data type from array to string for better visual

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,12 +1,13 @@
 
 function ProjectCard({project}) {
-    const {title, image, github_link, live_site} = project
+    const {title, image, github_link, live_site, description, tech_stack} = project
     console.log(project)
     return (
         <div className="card">
             <h1>{title}</h1>
             <img className="card-image" src={image} alt={title}/>
-            <p>description of project</p>
+            <p>{description}</p>
+            <p>Tech Stack: {tech_stack}</p>
             <a href={github_link}>GitHub</a>
             <a href={live_site}>Deployed Site</a>
         </div>
--- src/portfolioData.js
+++ src/portfolioData.js
@@ -16,7 +16,7 @@ const project_data = [
         title: "Cool Cat Store",
         image: "https://user-images.githubusercontent.com/107723341/266446077-e3c59c90-15ce-4196-8e83-e96fd19a4b1a.png",
         description: " E-commerce app built with React and Rails, showcasing many-to-many data relationships, CRUD features, and API integrations.",
-        tech_stack: ["Redux", "Stripe", "Vite", "Ruby", "Javascript", "React", "Ruby on Rails", "PostgreSQL", "Render"],
+        tech_stack: "Redux, Stripe, Vite, Ruby, Javascript, React, Ruby on Rails, PostgreSQL, Render",
         github_link: "https://github.com/JamesSalcedo001/cool_cat_store",
         live_site: "https://cool-cat-store.onrender.com/"
     },
@@ -25,7 +25,7 @@ const project_data = [
         title: "Space Base, the Social App",
         image: "https://user-images.githubusercontent.com/107723341/243157331-7e9590d6-814b-4165-99e7-c8bd01f15b1a.png",
         description: "Social media application designed to demonstrate mastery of React and Rails in building interactive platforms. The project features CRUD operations, multiple data models, and secure authentication.",
-        tech_stack: ["Ruby", "Javascript", "React", "Ruby on Rails", "PostgreSQL", "Render"],
+        tech_stack: "Ruby, Javascript, React, Ruby on Rails, PostgreSQL, Render",
         github_link: "https://github.com/JamesSalcedo001/social_app",
         live_site: "https://social-app-jpxa.onrender.com/"
     },
