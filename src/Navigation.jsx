179a1b39bdc4ac305c2aadfc445280f2104a7b57
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-09-22 16:09:41 -0500

    working on dropdown menu

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,12 +1,25 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { LuMenuSquare} from "react-icons/lu"
+import { CgCloseR } from "react-icons/cg"
 
 function Navigation() {
-    return (
-        <div id="nav-bar">
-            <NavLink to="/james_salcedo" className="nav-links">Home</NavLink>
-            <NavLink to="/james_salcedo/projects_list" className="nav-links">Projects</NavLink>
-        </div>
-    )
+    const [isShown, setIsShown] = useState(false)
+
+
+        return (
+            <div id="nav-container" className={isShown ? "open" : ""}>
+                <button id="menu-button" onClick={() => setIsShown(!isShown)}>
+                    {isShown ? <CgCloseR size={50}/> : <LuMenuSquare size={50}/>}
+                </button>
+                {isShown && (
+                    <div id="nav-bar">
+                         <NavLink to="/james_salcedo" className="nav-links">Home</NavLink>
+                         <NavLink to="/james_salcedo/projects_list" className="nav-links">Projects</NavLink>
+                    </div>
+                )}
+            </div>
+        )
 }
 
 export default Navigation;
