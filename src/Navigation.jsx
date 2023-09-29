6df0add4c40c2356ded40c5ec1ad38be6fbe0fa0
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-09-28 19:33:32 -0500

    smoothed out animation for opening and closing menu

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
-import { LuMenuSquare} from "react-icons/lu"
-import { CgCloseR } from "react-icons/cg"
+import { LuMenuSquare} from "react-icons/lu";
+import { CgCloseR } from "react-icons/cg";
 
 function Navigation() {
     const [isShown, setIsShown] = useState(false)
@@ -10,14 +10,12 @@ function Navigation() {
         return (
             <div id="nav-container" className={isShown ? "open" : ""}>
                 <button id="menu-button" onClick={() => setIsShown(!isShown)}>
-                    {isShown ? <CgCloseR size={39}/> : <LuMenuSquare size={42}/>}
+                    {isShown ? <CgCloseR size={42}/> : <LuMenuSquare size={42}/>}
                 </button>
-                {isShown && (
                     <div id="nav-bar">
                          <NavLink to="/james_salcedo" className="nav-links">Home</NavLink>
                          <NavLink to="/james_salcedo/projects_list" className="nav-links">Projects</NavLink>
                     </div>
-                )}
             </div>
         )
 }
