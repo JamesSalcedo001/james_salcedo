99d657dbee4bc75e525ce208b3103422581a7913
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-12-03 20:12:41 -0600

    changed image location

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -3,7 +3,7 @@ import resumePDF from "./James_Salcedo_Resume (1).pdf"
 const about_me = {
     name: "James Salcedo",
     email: "jamesa.salcedo3@gmail.com",
-    photo: "https://media.licdn.com/dms/image/D5603AQHJ7bDWd-gD8A/profile-displayphoto-shrink_800_800/0/1694925885801?e=1700697600&v=beta&t=l0Fpob83xPQCw96CdUh9akvh7uJkClJ_7izyF6Ed_9k",
+    photo: "src/Screen Shot 2023-12-03 at 8.10.16 PM.png",
     bio: "I am a full stack developer passionate about creation, innovation, and collaboration. I am especially interested in the fields of technology and gaming, and have a strong desire to make a significant impact in driving the future.",
     linkedIn: "https://www.linkedin.com/in/james-antonio-salcedo/",
     github: "https://github.com/JamesSalcedo001",
