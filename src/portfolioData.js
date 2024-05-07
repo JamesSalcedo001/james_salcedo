49a4dfc22be1af55cae15c6fb5fc012c9291557a
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-05-07 18:25:47 -0500

    Add newest project to portfolio data

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -22,7 +22,7 @@ const project_data = [
         title: "Cool Cat Store",
         image: "https://user-images.githubusercontent.com/107723341/266446077-e3c59c90-15ce-4196-8e83-e96fd19a4b1a.png",
         description: " E-commerce app built with React and Rails, showcasing many-to-many data relationships, CRUD features, and API integrations.",
-        tech_stack: "Redux, Stripe, Vite, Ruby, Javascript, React, Ruby on Rails, PostgreSQL, Render",
+        tech_stack: "Redux, Stripe, Vite, Ruby, JavaScript, React, Ruby on Rails, PostgreSQL, Render",
         github_link: "https://github.com/JamesSalcedo001/cool_cat_store",
         live_site: "https://cool-cat-store.onrender.com/"
     },
@@ -31,9 +31,18 @@ const project_data = [
         title: "Space Base, the Social App",
         image: "https://user-images.githubusercontent.com/107723341/243157331-7e9590d6-814b-4165-99e7-c8bd01f15b1a.png",
         description: "Social media application designed to demonstrate mastery of React and Rails in building interactive platforms. The project features CRUD operations, multiple data models, and secure authentication.",
-        tech_stack: "Ruby, Javascript, React, Ruby on Rails, PostgreSQL, Render",
+        tech_stack: "Ruby, JavaScript, React, Ruby on Rails, PostgreSQL, Render",
         github_link: "https://github.com/JamesSalcedo001/social_app",
         live_site: "https://social-app-jpxa.onrender.com/"
+    },
+    {
+        id: 3,
+        title: "The Sweet Bandit",
+        image: "./src/images/Screen Shot 2024-05-07 at 5.58.52 PM.png",
+        description: "Official Site for The Sweet Bandit local business. Currently working with client to deliver further features to the site.",
+        tech_stack: "Redux, Stripe, Vite, Ruby, JavaScript, React, Ruby on Rails, PostgreSQL, Render, Jira",
+        github_link: "https://github.com/JamesSalcedo001/sweet_bandit",
+        live_site: "https://sweet-bandit.onrender.com/"
     }
 ]
 
