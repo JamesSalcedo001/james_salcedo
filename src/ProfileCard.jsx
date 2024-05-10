58e628e55057923e3bc88d1cc72f6ec14eb32eb5
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-05-10 14:35:04 -0500

    Major revisions on image sizing, font size and weight, alter border radius proportions, add box shadow to profile card for more uniform design

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -2,10 +2,11 @@ import { about_me } from "./portfolioData"
 function ProfileCard() {
     const {name, photo, bio, linkedIn, github, blog, resumePDF, email} = about_me
     return (
+        <>
         <div id="profile-card">
             <img id="profile-image" src={photo} alt="my photo"/>
             <h1>{name}</h1>
-            <p>{bio}</p>
+            <p id="pf-bio">{bio}</p>
             <h5>Technical links!</h5>
             <a href={blog} className="button" target="_blank" rel="noopener noreferrer">Blog</a>
             <a href={github} className="button" target="_blank" rel="noopener noreferrer">GitHub</a>
@@ -15,6 +16,7 @@ function ProfileCard() {
             <a id="linkedin-tag" href={linkedIn} target="_blank" rel="noopener noreferrer">LinkedIn</a>
             <h5 id="email">Email: {email}</h5>
         </div>
+        </>
     )
 }
 
