1cce012c886bbb35ce8a22dcc8bf3b5f35589e9e
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-05-10 15:36:20 -0500

    Split profile card into two separate cards to prevent crowded card visual

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -3,19 +3,25 @@ function ProfileCard() {
     const {name, photo, bio, linkedIn, github, blog, resumePDF, email} = about_me
     return (
         <>
-        <div id="profile-card">
+        <div id="top-profile-card">
             <img id="profile-image" src={photo} alt="my photo"/>
             <h1>{name}</h1>
             <p id="pf-bio">{bio}</p>
-            <h5>Technical links!</h5>
+        </div>
+        <div id="profile-card">
+            {/* <img id="profile-image" src={photo} alt="my photo"/>
+            <h1>{name}</h1>
+            <p id="pf-bio">{bio}</p> */}
+            <h2>Technical links!</h2>
             <a href={blog} className="button" target="_blank" rel="noopener noreferrer">Blog</a>
             <a href={github} className="button" target="_blank" rel="noopener noreferrer">GitHub</a>
             <a href={resumePDF} className="button" target="_blank" rel="noopener noreferrer">Resume</a>
 
-            <h5 id="contact-me">Contact me!</h5>
+            <h2 id="contact-me">Contact me!</h2>
             {/* <a id="linkedin-tag" href={linkedIn} target="_blank" rel="noopener noreferrer">LinkedIn</a> */}
             <a className="button" href={linkedIn} target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            <h5 id="email">Email: {email}</h5>
+            <h2 id="email">Email</h2>
+            <p>{email}</p>
         </div>
         </>
     )
